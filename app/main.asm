5b9f4d7c3fbd2472b6cb540c2fb8b48ae5159733
AlexDeadmond <alexdeadmond733@gmail.com>
2025-01-20 17:02:13 -0700

    Edited timing for LED

--- app/main.asm
+++ app/main.asm
@@ -84,7 +84,7 @@ SetupP1     bic.b   #BIT0,&P1OUT            ; Clear P1.0 output
             bic.w   #LOCKLPM5,&PM5CTL0      ; Unlock I/O pins
 
 Mainloop    xor.b   #BIT0,&P1OUT            ; Toggle P1.0 every 0.1s
-Wait        mov.w   #5000000,R15              ; Delay to R15
+Wait        mov.w   #500000,R15              ; Delay to R15
 L1          dec.w   R15
             jnz     L1
             jmp     Mainloop                ; Again
